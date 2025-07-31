b9b138f7b580bd8711efc5a43fa41d0952addd2c
AryanP1617 <punalekarar@gmail.com>
2025-07-31 20:20:33 +0530

    Added css to make the site interactive

--- index.js
+++ index.js
@@ -1,41 +1,44 @@
 let choice,computer_choice;
-let attempts=3;
-let player_win=0,computer_win=0;
 
-while(attempts>0)
+
+document.querySelectorAll(".buttons").forEach(button => {
+    button.addEventListener("click",()=>
+    {
+        let choice=parseInt(button.dataset.choice);
+        playround(choice)
+    });
+});
+
+document.getElementById("resetbutton").addEventListener("click",()=>{
+    document.getElementById("display").textContent = ""
+})
+function playround(choice)
 {
-    choice=Number(prompt("Enter 1 for rock 2 for paper and 3 for scissors"));
-    computer_choice=Math.floor(Math.random()*3)
+    computer_choice=Math.floor(Math.random()*3)+1;
+    let parentNode=document.getElementById("display");
+    let childNode=document.createElement("p");
     if(choice==computer_choice)
     {
-        console.log("It is a tie");
+        
+        childNode.textContent="It is a tie";
+        
+        
     }
     else
     {
         if((choice==1 && computer_choice==3)||(choice==2 && computer_choice==1)||(choice==3 && computer_choice==2))
         {
-            console.log(`The player choice:${choice}\nThe computer choice:${computer_choice}\nThe player wins this round`);
-            player_win++;
+            
+            childNode.textContent="Player wins";
+            
         }
         else if((computer_choice==1 && choice==3)||(computer_choice==2 && choice==1)||(computer_choice==3 && choice==2))
         {
-            console.log(`The player choice:${choice}\nThe computer choice:${computer_choice}\nThe computer wins this round`);
-            computer_win++;   
+            
+            childNode.textContent="Computer wins";
+            
         }
     }
-    attempts--;
-
+    parentNode.appendChild(childNode);    
 }
 
-if(player_win>computer_win)
-{
-    console.log("The player wins");
-}
-else if(player_win<computer_win)
-{
-    console.log("The computer wins");
-}
-else
-{
-    console.log("It is a tie");
-}
